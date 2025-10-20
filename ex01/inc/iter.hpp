7fc1641f8f8c24f679b2976b921b943a01f10d12
Cedric Bauer <cbauer@3-C-7.42heilbronn.de>
2025-10-20 10:50:31 +0200

    figuring out the iter func

---
+++ ex01/inc/iter.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <cstddef>
+
+template <typename t, typename f>
+
+void    iter( t *array, const t lenght, t &func ) // for non const
+{
+    //  a == address of an array
+    //  b == length of the array, const
+    //  c == function that will be called in every element of the array
+
+    for ( size_t i = 0; i < lenght; i++ )
+    {
+        c( &a[i] );
+    }
+}
+
+template <typename t, typename f>
+
+void    iter( const t *array, const t length, f &func ) //overload for const
+{
+    //  a == address of an array
+    //  b == length of the array, const
+    //  c == function that will be called in every element of the array
+
+    for ( size_t i = 0; i < length; i++ )
+    {
+        c( const array[i] );
+    }
+}
+
+Subject:
+
+Chapter V
+Exercise 01: Iter
+Exercise: 01
+Iter
+Directory: ex01/
+Files to Submit: Makefile, main.cpp, iter.{h, hpp}
+Forbidden: None
+Implement a function template iter that takes 3 parameters and returns nothing.
+• The first parameter is the address of an array.
+• The second one is the length of the array, passed as a const value.
+• The third one is a function that will be called on every element of the array.
+Submit a main.cpp file that contains your tests. Provide enough code to generate a
+test executable.
+Your iter function template must work with any type of array. The third parameter
+can be an instantiated function template.
+The function passed as the third parameter may take its argument by const reference
+or non-const reference, depending on the context.
+Think carefully about how to support both const and non-const
+elements in your iter function.
