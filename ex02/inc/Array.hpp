4479f5f43324dacad0b349d899755bfda297fe1c
Cedric Bauer <cbauer@3-C-7.42heilbronn.de>
2025-10-24 12:35:36 +0200

    building and understanding how to write cannonical form for templates; taking me longer than expected

--- ex02/inc/Array.hpp
+++ ex02/inc/Array.hpp
@@ -12,31 +12,50 @@ private:
     unsigned int    _size;
     T*              _array;
 public:
-    Array() : _size( 0 ), _array( nullptr ) {}
-    Array( unsigned int n ) : _size( n ), _array( new T[n]() ) {}
-    Array( const Array& copy ) : _size( copy._size), _array( new T[copy._size]() )
+    Array() : _size( 0 ), _array( nullptr ) { std::cout << "\e[32mDefault Array constructor!\e[0m" << std::endl; }
+    Array( unsigned int n ) : _size( n ), _array( n ? new T[n]() : nullptr ) {std::cout << "\e[32mParametric Array constructor!\e[0m" << std::endl;}
+    Array( const Array& copy ) : _size( copy._size ), _array( copy._size ? new T[copy._size]() : nullptr )
     {
-        if (copy._size == 0)
-        {
-            this->_array = nullptr;
-        }
-        // Array other( copy._size, copy._array );
+        std::cout << "\e[31mCopy Array constructor!\e[0m" << std::endl;
         for ( unsigned int i = 0; i < copy._size; i++ )
-            this._array[ i ] = copy._array[ i ];
+            this->_array[ i ] = copy._array[ i ];
     }
 
 
     Array& operator=( const Array& copy )
     {
-        if ( this != &copy )
+        if ( this != &copy ) // Selbstzuweisungscheck
         {
-            this->size = copy.size;
-            this->_array = copy._array;
+            //neuen Speicher temporaer allokieren in Pointer
+            // aber ich weiss nicht ob ich hier:  machen soll
+            T* temp = copy._size ? new T[copy._size]() : nullptr;
+            try
+            {
+                for ( unsigned int i = 0; i < copy._size; ++i )
+                    temp[ i ] = copy._array[ i ];
+            }
+            catch(...) //cathces all exception types
+            {
+                delete[] temp;
+                throw ;
+            }
+            
+            delete[] _array;
+            this->_array = temp;  //Inhalt kopieren
+            this->_size = copy._size; //Inhalt kopieren
+            return( *this ); //alten speicher loeschen
         }
-        return ( *this );
+        return( *this );
     }
-    ~Array();
-
-    static unsigned int size(); //returns the size of the array -- number of elements
+    ~Array()
+    {
+        std::cout << "\e[31mArray destructor\e[0m" << std::endl;
+        delete[] _array;
+    }
+    
+    unsigned int size() const
+    {
+        return ( this->_size );
+    } //returns the size of the array -- number of elements
 
 };
